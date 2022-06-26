88fef55a834bd197cf8495603a356b659a16ee31
MOPa - CAS - João Vitor Silva Dutra <joao.dutra@unb.educadventista.org>
2022-06-26 13:39:10 -0300

    Adicionando tela de interção com usuário e funcionalidades

--- Python/Gerador de senhas/gerador_de_senhas.py
+++ Python/Gerador de senhas/gerador_de_senhas.py
@@ -1,4 +1,14 @@
 import random
+import PySimpleGUI as sg
+import pyperclip as pc
+
+layout = [
+    [sg.Text('Escolha um número de caracteres para sua senha:')],
+    [sg.Input(key = '-INPUT-'), sg.Button('Gerar', key = '-GERAR-')],
+    [sg.Text('Senha', key = '-SENHA-'), sg.Button('Copiar', key = '-COPIAR-'), sg.Text('status', key = '-STATUS-')]
+]
+
+window = sg.Window('Gerador de Senhas', layout)
 
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -6,8 +16,24 @@ number = "0123456789"
 symbols = "!@#$%¨&*()<>,.\|-_*/+=[]}{"
 
 Use_for = lower_case + upper_case + number + symbols 
-length_for_pass = 9
+length_for_pass = 0
 
-password = "".join(random.sample(Use_for, length_for_pass))
+while True:
+    event, values = window.read()
 
-print("Sua senha gerada via Python é: " + password)
+    if event == sg.WIN_CLOSED:
+        break
+    
+    if event == '-GERAR-':
+        input_value = values['-INPUT-']
+        if input_value.isnumeric():
+            length_for_pass = int(input_value)
+            password = "".join(random.sample(Use_for, length_for_pass))
+            window['-SENHA-'].update("Sua senha gerada por Python é: " + password)
+        else:
+            window['-STATUS-'].update("Insira um valor numérico!")
+        
+
+    if event == '-COPIAR-':
+        pc.copy(password)
+        window['-STATUS-'].update("copiado")
